f963a03715694b715a3beb01d9a5b07a9c4c4cfa
manikantan <manikantan@applab.qa>
2023-06-23 12:05:57 +0300

    test push

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist') 
   },
   module: {
     rules: [
